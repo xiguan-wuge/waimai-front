91b60412e2fb7545b48b1e4858685105fb531a17
xiguan-wuge <33884004+xiguan-wuge@users.noreply.github.com>
2023-08-12 04:12:36 +0000

    Deploying to gh-pages from @ xiguan-wuge/waimai-front@c269620a89abb8dbcfba0c3be6e3a94f4bbf14cb 🚀

--- src/api/apiMap.ts
+++
@@ -1,8 +0,0 @@
-type apiMapType = {
-  [propName: string]: string
-}
-export const apiMap:apiMapType = {
-  getFoodList: 'get/foodlist',
-  getShopcartList: 'get/shopcart/list',
-  getMyOrder: 'get/myorder',
-}
